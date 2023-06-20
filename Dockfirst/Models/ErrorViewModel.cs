c08c81e838012ff177f485295fb6facbebf41b7b
TamerlanHasanov124 <hasanov.tamerlan.96@gmail.com>
2023-06-20 23:29:03 +0400

    stradaniye

---
+++ Dockfirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dockfirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
